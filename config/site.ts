e0ac42839dba0d36f75dabfaee755f371f882e01
Alan Shalem <alanshalem@gmail.com>
2024-04-27 15:27:42 -0300

    site header

---
+++ config/site.ts
@@ -0,0 +1,13 @@
+export const siteConfig = {
+  name: "Alan Shalem Blog",
+  url: "https://alanshalem.io",
+  description: "Nextjs 14 blog using velite, tailwindcss and shadcn",
+  author: "alanshalme",
+  links: {
+    twitter: "https://twitter.com/alanshalem_",
+    github: "https://github.com/alanshalem",
+    personalSite: "https://alanshalem.vercel.app",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
