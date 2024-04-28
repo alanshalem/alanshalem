3e8a6ea5d31bcc8734eff32adeb450fc83c53cb6
Alan Shalem <alanshalem@gmail.com>
2024-04-28 20:00:52 -0300

    add tags pages

--- components/project-item.tsx
+++ components/project-item.tsx
@@ -23,7 +23,7 @@ const ProjectItem = ({
     <article className="flex flex-col gap-2 border-border border-b py-3">
       <div>
         <h2 className="text-2xl font-bold">
-          <Link href={slug}>{title}</Link>
+          <Link href={"/" + slug}>{title}</Link>
         </h2>
       </div>
 
